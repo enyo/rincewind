3d2e59a37818f6e1e1be85d3722f181f7351941c
Matthias Loitsch <m@tthias.com>
2010-03-04 10:30:44 +0100

    Added Dao Type checks

---
+++ tests/library/Dao/DaoTests.php
@@ -0,0 +1,35 @@
+<?php
+
+
+	require_once(dirname(dirname(__FILE__)) . '/setup.php');
+
+	require_once(LIBRARY_ROOT_PATH . 'Dao/Dao.php');
+
+
+	class DaoWithDatabaseTest extends Snap_UnitTestCase {
+
+	    public function setUp() {
+    	}
+
+	    public function tearDown() {
+	    }
+
+	    public function testIntIdenticalInteger() {
+	        return $this->assertIdentical(Dao::INT, Dao::INTEGER);
+	    }
+
+	    public function testBoolIdenticalBoolean() {
+	        return $this->assertIdentical(Dao::BOOL, Dao::BOOLEAN);
+	    }
+
+	    public function testTimestampIdenticalDateWithTime() {
+	        return $this->assertIdentical(Dao::TIMESTAMP, Dao::DATE_WITH_TIME);
+	    }
+
+	    public function testStringIdenticalText() {
+	        return $this->assertIdentical(Dao::STRING, Dao::TEXT);
+	    }
+
+	}
+
+?>

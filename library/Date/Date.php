ce4f3a7d95884684758de4f8e3933ea78f94b402
Matthias Loitsch <m@tthias.com>
2010-03-04 21:49:28 +0100

    Added correct phpdoc data to Date and GlobalStorage

--- library/Date/Date.php
+++ library/Date/Date.php
@@ -1,71 +1,94 @@
 <?php
 
+/**
+ * This file contains the definition for a Date.
+ *
+ * @author Matthias Loitsch <developer@ma.tthias.com>
+ * @copyright Copyright (c) 2010, Matthias Loitsch
+ * @package Date
+ **/
 
-	class DateException extends Exception { }
+
+/**
+ * The Date Exception
+ *
+ * @author Matthias Loitsch <developer@ma.tthias.com>
+ * @copyright Copyright (c) 2010, Matthias Loitsch
+ * @package Date
+ * @subpackage Exceptions
+ **/
+class DateException extends Exception { }
+
+
+/**
+ * The Date object is used to store the date and time.
+ * I tried extending the built in DateTime object, but overriding the constructor yielded unpleasant results.
+ *
+ * @author Matthias Loitsch <developer@ma.tthias.com>
+ * @copyright Copyright (c) 2010, Matthias Loitsch
+ * @package Date
+ */
+class Date {
 
 	/**
-	 * The Date object is used to store the date and time.
-	 * I tried extending the built in DateTime object, but overriding the constructor yielded unpleasant results.
+	 * This format is used with date() when formatting the date.
+	 * Use setFormat() to overwrite it.
 	 *
-	 * @author     Matthias Loitsch <develop@matthias.loitsch.com>
-	 * @copyright  Copyright (c) 2009, Matthias Loitsch
+	 * @var string
 	 */
-	class Date {
+	protected $defaultFormat = 'Y-m-d H:i:s';
 	
-		/**
-		 * This format is used with date() when formatting the date.
-		 * Use setFormat() to overwrite it.
-		 *
-		 * @var string
-		 */
-		protected $defaultFormat = 'Y-m-d H:i:s';
-		
-		/**
-		 * The time of the date object.
-		 *
-		 * @var int
-		 */
-		protected $timestamp;
+	/**
+	 * The time of the date object.
+	 *
+	 * @var int
+	 */
+	protected $timestamp;
 
-		/**
-		 * The time is specified in the constructor
-		 *
-		 * @param mixed $time Integer for a timestamp, String for strtotime() or null for current time.
-		 **/
-		public function __construct($time = null) {
-			if (!$time)                                        $this->timestamp = time();
-			elseif (is_numeric($time) && $time == (int) $time) $this->timestamp = (int) $time;
-			else                                               $this->timestamp = strtotime($time);
-			if (!$this->timestamp) throw new DateException('This date could not be converted: "' . $time . '"');
-		}
-
-		/**
-		 * Formats the timestamp, and returns it.
-		 *
-		 * @param string $format The format to be used (the string passed to date()). If empty $this->defaultFormat is used.
-		 **/
-		public function format($format = null) {
-			return date($format === null ? $this->defaultFormat : $format, $this->timestamp);
-		}
-
-
-
-		/**
-		 * Simply returns the timestamp.
-		 */
-		public function getTimestamp() { return $this->timestamp; }
-
-		/**
-		 * @param string $format Sets the defaultFormat.
-		 */
-		public function setFormat($format) { $this->defaultFormat = $format; }
-
-
-
-
-		public function __toString() { return $this->format(); }
-	
+	/**
+	 * The time is specified in the constructor
+	 *
+	 * @param int|string $time Integer for a timestamp, String for strtotime() or null for current time.
+	 */
+	public function __construct($time = null) {
+		if (!$time)                                        $this->timestamp = time();
+		elseif (is_numeric($time) && $time == (int) $time) $this->timestamp = (int) $time;
+		else                                               $this->timestamp = strtotime($time);
+		if (!$this->timestamp) throw new DateException('This date could not be converted: "' . $time . '"');
 	}
-	
+
+	/**
+	 * Formats the timestamp, and returns it.
+	 *
+	 * @param string $format The format to be used (the string passed to date()). If empty $this->defaultFormat is used.
+	 * @return string The formatted string
+	 */
+	public function format($format = null) {
+		return date($format === null ? $this->defaultFormat : $format, $this->timestamp);
+	}
+
+
+
+	/**
+	 * Simply returns the timestamp.
+	 * @return int
+	 */
+	public function getTimestamp() { return $this->timestamp; }
+
+
+	/**
+	 * @param string $format Sets the defaultFormat.
+	 */
+	public function setFormat($format) { $this->defaultFormat = $format; }
+
+	/**
+	 * Calls format()
+	 *
+	 * @see format()
+	 */
+	public function __toString() { return $this->format(); }
+
+}
+
 
 ?>
--- library/Storage/GlobalStorage.php
+++ library/Storage/GlobalStorage.php
@@ -1,36 +1,57 @@
 <?php
 
+/**
+ * This file contains the definition for the global Storage.
+ *
+ * @author Matthias Loitsch <developer@ma.tthias.com>
+ * @copyright Copyright (c) 2010, Matthias Loitsch
+ * @package Storage
+ **/
+
+
+/**
+ * This is the global storage.
+ * Some objects are already implemented for common usage.
+ * If you need other / more stuff to store, extend this storage.
+ * All functions in this class have to be static.
+ *
+ * @author Matthias Loitsch <developer@ma.tthias.com>
+ * @copyright Copyright (c) 2010, Matthias Loitsch
+ * @package Storage
+ */
+class GlobalStorage {
 
 	/**
-	 * @author     Matthias Loitsch <develop@matthias.loitsch.com>
-	 * @copyright  Copyright (c) 2009, Matthias Loitsch
+	 * A profiler to be used anywhere in the application.
 	 *
-	 * This is the global storage.
-	 * Some objects are already implemented, because other library classes may try
-	 * to access them (eg: the profiler).
-	 * If you need other / more stuff to store, extend this storage.
-	 *
+	 * @var Profiler $profiler
 	 */
-	class GlobalStorage {
+	static protected $profiler;
 
-		/**
-		 * A profiler to be used anywhere in the application.
-		 *
-		 * @var Profiler $profiler
-		 */
-		static protected $profiler;
-
-
-		public static function setProfiler($profiler) { self::$profiler = $profiler; }
-		
-		public static function getProfiler() { return self::$profiler; }
-
-
-
-		public function __construct() { die('The storage is not ment to be instantiated.'); }
-
+	/**
+	 * Sets the profiler
+	 *
+	 * @param Profiler $profiler
+	 */
+	public static function setProfiler($profiler) {
+		self::$profiler = $profiler;
+	}
+	
+	/**
+	 * Returns the profiler
+	 *
+	 * @return Profiler
+	 * @see setProfiler
+	 */
+	public static function getProfiler() {
+		return self::$profiler;
 	}
 
+	/**
+	 * Trying to instanciate the class aborts the script.
+	 */
+	public function __construct() { die('The storage is not ment to be instantiated.'); }
 
+}
 
 ?>

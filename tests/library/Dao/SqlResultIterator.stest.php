2dd75827876ec2369a194a44309a53bb21df449b
Matthias Loitsch <m@tthias.com>
2010-03-09 18:37:47 +0100

    Added tests for the SqlResultIterator

--- tests/library/Dao/SqlResultIterator.stest.php
+++ tests/library/Dao/SqlResultIterator.stest.php
@@ -3,35 +3,76 @@
 
 	require_once(dirname(dirname(__FILE__)) . '/setup.php');
 
-	require_once(LIBRARY_ROOT_PATH . 'Dao/Dao.php');
+	require_once(LIBRARY_ROOT_PATH . 'Dao/SqlResultIterator.php');
+	require_once(LIBRARY_ROOT_PATH . 'Dao/DaoInterface.php');
+	require_once(LIBRARY_ROOT_PATH . 'DatabaseResult/DatabaseResultInterface.php');
+
 
 
 	class SqlResultIterator_Test extends Snap_UnitTestCase {
 
+		protected $numRows = 88;
+
+		protected $iterator;
+		protected $result;
+		protected $dao;
+
+		protected $dataObject = 'abcdefg';
+
 	    public function setUp() {
+			$this->result = $this->mock('DatabaseResultInterface')
+				->setReturnValue('numRows', $this->numRows)
+				->setReturnValue('fetchArray', array())
+				->listenTo('fetchArray')
+				->construct();
+			$this->dao = $this->mock('DaoInterface')
+				->setReturnValue('getObjectFromDatabaseData', $this->dataObject)
+				->construct();
+			$this->iterator = new SqlResultIterator($this->result, $this->dao);
     	}
 
 	    public function tearDown() {
+			unset($this->iterator);
 	    }
 
 	    public function testCount() {
-			return $this->todo();
+			return $this->assertEqual($this->iterator->count(), $this->numRows);
 	    }
 
-	    public function testNext() {
-			return $this->todo();
+	    public function testKey() {
+			return $this->assertEqual($this->iterator->key(), 1);
 	    }
 
-	    public function testRewind() {
-			return $this->todo();
+		public function testCurrentLeavesKeyTo1() {
+			$this->iterator->current();
+			return $this->assertEqual($this->iterator->key(), 1);
+		}
+
+		public function testNextSetsKeyTo2() {
+			$this->iterator->next();
+			return $this->assertEqual($this->iterator->key(), 2);
+		}
+
+	    public function testRewindSetsKeyTo1() {
+			$this->iterator->next();
+			$this->iterator->next();
+			$this->iterator->next();
+			$this->iterator->rewind();
+			return $this->assertEqual($this->iterator->key(), 1);
 	    }
+
+
+		public function testCurrentCallsFetchArray() {
+			$this->iterator->current();
+			return $this->assertCallCount($this->result, 'fetchArray', 1);
+		}
 
 	    public function testNextReturnsItself() {
-			return $this->todo();
+			return $this->assertIdentical($this->iterator->next(), $this->iterator);
 	    }
 
 	    public function testRewindReturnsItself() {
-			return $this->todo();
+			return $this->assertIdentical($this->iterator->rewind(), $this->iterator);
 	    }
 
 	}

744ae35217ff025c041c2bc2af8f454a0523e373
Matthias Loitsch <m@tthias.com>
2010-06-02 17:35:54 +0200

    Added a JsonResultIterator test

---
+++ tests/library/Dao/JsonResultIterator.stest.php
@@ -0,0 +1,60 @@
+<?php
+
+
+require_once(dirname(dirname(__FILE__)) . '/setup.php');
+
+require_once(LIBRARY_ROOT_PATH . 'Dao/Json/JsonDao.php');
+
+
+
+class JsonResultIterator_Basic_Test extends Snap_UnitTestCase {
+
+  protected $dao;
+
+  public function setUp() {
+    $this->dao = $this->mock('Dao')
+      ->listenTo('getObjectFromData')
+      ->listenTo('getObjectFromData', array(new Snap_Equals_Expectation(array('some1'=>'hash'))))
+      ->listenTo('getObjectFromData', array(new Snap_Equals_Expectation(array('some2'=>'hash'))))
+      ->listenTo('getObjectFromData', array(new Snap_Equals_Expectation(array('some3'=>'hash'))))
+      ->construct(null);
+    $this->it = new JsonResultIterator(array(array('some1'=>'hash'), array('some2'=>'hash'), array('some3'=>'hash')), $this->dao);
+  }
+
+  public function tearDown() { }
+
+  public function testLength() {
+    return $this->assertIdentical($this->it->count(), 3);
+  }
+
+  public function testNothingFetchedUntilNeeded() {
+    return $this->assertCallCount($this->dao, 'getObjectFromData', 0);
+  }
+
+  public function testAllHashesInstantiated() {
+    foreach ($this->it as $test) { }
+    return $this->assertCallCount($this->dao, 'getObjectFromData', 3);
+  }
+
+  public function testFirstHash() {
+    foreach ($this->it as $test) { }
+    return $this->assertCallCount($this->dao, 'getObjectFromData', 1, array(new Snap_Equals_Expectation(array('some1'=>'hash'))));
+  }
+
+  public function testSecondHash() {
+    foreach ($this->it as $test) { }
+    return $this->assertCallCount($this->dao, 'getObjectFromData', 1, array(new Snap_Equals_Expectation(array('some2'=>'hash'))));
+  }
+
+  public function testThirdHash() {
+    foreach ($this->it as $test) { }
+    return $this->assertCallCount($this->dao, 'getObjectFromData', 1, array(new Snap_Equals_Expectation(array('some3'=>'hash'))));
+  }
+
+
+}
+
+
+
+
+?>

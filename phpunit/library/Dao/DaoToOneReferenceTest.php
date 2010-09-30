7843db81f567f3ff31020066f2e7b748cc25cdb3
Matthias Loitsch <m@tthias.com>
2010-09-30 19:59:35 +0200

    The DaoToOneReference has now 100% code coverage.

--- phpunit/library/Dao/DaoToOneReferenceTest.php
+++ phpunit/library/Dao/DaoToOneReferenceTest.php
@@ -15,11 +15,11 @@ class DaoToOneReferenceTest extends PHPUnit_Framework_TestCase {
    * @var Dao
    */
   protected $dao;
-
   /**
    * @var Record
    */
   protected $record;
+
   /**
    * Sets up the fixture, for example, opens a network connection.
    * This method is called before a test is executed.
@@ -34,7 +34,7 @@ class DaoToOneReferenceTest extends PHPUnit_Framework_TestCase {
   public function testReferenceJustInstantiatesRecordIfDataPresent() {
     $this->getMockForAbstractClass('Dao', array('createDao'), 'NotAbstractDao', false);
     $this->dao = $this->getMock('NotAbstractDao', array('getRecordFromData'), array(), '', false);
-    $recordData = array('id'=>1, 'name'=>'strasse');
+    $recordData = array('id' => 1, 'name' => 'strasse');
     $this->record->expects($this->once())->method('getDirectly')->with('address')->will($this->returnValue($recordData));
     $this->dao->expects($this->once())->method('getRecordFromData')->with($recordData)->will($this->returnValue('RECORD'));
 
@@ -58,8 +58,6 @@ class DaoToOneReferenceTest extends PHPUnit_Framework_TestCase {
     self::assertSame('RECORD', $reference->getReferenced($this->record, 'address'));
   }
 
-
-
   /**
    * @covers DaoToOneReference::getReferenced
    */
@@ -72,6 +70,57 @@ class DaoToOneReferenceTest extends PHPUnit_Framework_TestCase {
     self::assertNull($reference->getReferenced($this->record, 'address'));
   }
 
+  /**
+   * @covers DaoToOneReference::getReferenced
+   */
+  public function testReferenceReturnsNullIfLocalAndForeignKeysNotSet() {
+    $this->record->expects($this->exactly(2))->method('getDirectly')->with('address')->will($this->returnValue(null));
 
+    $reference = new DaoToOneReference($this->dao, 'localKey', null);
+    self::assertNull($reference->getReferenced($this->record, 'address'));
+    $reference = new DaoToOneReference($this->dao, null, 'foreignKey');
+    self::assertNull($reference->getReferenced($this->record, 'address'));
+  }
+
+  /**
+   * @covers DaoToOneReference::getReferenced
+   */
+  public function testReferenceGetsTheReferencedRecordAndReturnsIt() {
+    $this->getMockForAbstractClass('Dao', array('createDao'), 'NotAbstractDao3', false);
+    $dao = $this->getMock('NotAbstractDao3', array('get'), array(), '', false);
+
+    $this->record->expects($this->once())->method('getDirectly')->with('address')->will($this->returnValue(null));
+
+    $this->record->expects($this->once())->method('get')->with('localKey')->will($this->returnValue(1234));
+
+    $newRecord = $this->getMock('Record', array(), array(), '', false);
+    $dao->expects($this->once())->method('get')->with(array('foreignKey'=>1234))->will($this->returnValue($newRecord));
+    $newRecord->expects($this->once())->method('getArray')->will($this->returnValue(array('a', 'b')));
+
+    // make sure the records gets the data set.
+    $this->record->expects($this->once())->method('setDirectly')->with('address', array('a', 'b'));
+
+
+    $reference = new DaoToOneReference($dao, 'localKey', 'foreignKey');
+    self::assertSame($newRecord, $reference->getReferenced($this->record, 'address'));
+
+
+  }
+
+
+  /**
+   * @covers DaoToOneReference::getReferenced
+   */
+  public function testReferenceReturnsNullIfLocalValueIsNull() {
+    $this->getMockForAbstractClass('Dao', array('createDao'), 'NotAbstractDao4', false);
+    $dao = $this->getMock('NotAbstractDao4', array('get'), array(), '', false);
+
+    $this->record->expects($this->once())->method('getDirectly')->with('address')->will($this->returnValue(null));
+
+    $this->record->expects($this->once())->method('get')->with('localKey')->will($this->returnValue(null));
+
+    $reference = new DaoToOneReference($dao, 'localKey', 'foreignKey');
+    self::assertNull(null, $reference->getReferenced($this->record, 'address'));
+  }
 
 }

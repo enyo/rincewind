249c4b2b6a735e59d6a0ac30070e4d137126f7b4
Matthias Loitsch <m@tthias.com>
2010-10-22 14:32:43 +0200

    The Log now supports multiple loggers for a context.

--- library/Logger/Log.php
+++ library/Logger/Log.php
@@ -6,13 +6,11 @@
  * @author Matthias Loitsch <developer@ma.tthias.com>
  * @copyright Copyright (c) 2010, Matthias Loitsch
  * @package Logger
- **/
-
-
+ */
 /**
  * Including the LoggerExceptions
  */
-if (!class_exists('LogException', false)) include(dirname(__FILE__) . '/LoggerExceptions.php');
+if ( ! class_exists('LogException', false)) include dirname(__FILE__) . '/LoggerExceptions.php';
 
 /**
  * The Log class is abstract and only used with static function.
@@ -28,16 +26,17 @@ if (!class_exists('LogException', false)) include(dirname(__FILE__) . '/LoggerEx
  * ?>
  * </code>
  *
- * If you didn't set a logger for a specific context, it will be ignored unless you specified a CATCHALL logger.
+ * If you didn't set a logger for a specific context, it will be ignored unless you specified a Log::CATCHALL logger.
  *
  * If you don't pass a context, the context Log::GENERAL is used.
+ *
+ * You can pass multiple loggers for one context.
  *
  * @author Matthias Loitsch <developer@ma.tthias.com>
  * @copyright Copyright (c) 2010, Matthias Loitsch
  * @package Logger
- **/
+ */
 abstract class Log {
-
   /**
    * The string for the general logger.
    *
@@ -53,15 +52,13 @@ abstract class Log {
   const CATCHALL = ' CATCHALL ';
 
   /**
-   * Contains a list of loggers for each context.
+   * Contains a list of lists of loggers for each context.
    * You can add a logger with Log::addLogger()
+   *
    * @var array
    * @see addLogger
    */
   protected static $loggers = array();
-
-
-
   /**
    * This is a simple bool, to be able to check quickly if logging has been enabled or not.
    * If one logger gets added for any context this bool is set to true.
@@ -71,7 +68,6 @@ abstract class Log {
    * @var bool
    */
   protected static $isEnabled = false;
-
 
   /**
    * Adds a logger in the loggers array to be used in a certain context.
@@ -83,24 +79,25 @@ abstract class Log {
     if ($context !== self::GENERAL && $context !== self::CATCHALL && (empty($context) || preg_replace('/[^a-z0-9\_\-]/im', '', $context) != $context)) throw new LogException("The context name '$context' is not allowed.");
     self::$isEnabled = true;
     $context = self::sanitizeContext($context);
-    self::$loggers[$context] = $logger;
+    if ( ! array_key_exists($context, self::$loggers)) self::$loggers[$context] = array();
+    self::$loggers[$context][] = $logger;
   }
 
-
   /**
-   * Returns a logger for a specific context.
+   * Returns a list of logger for a specific context.
+   * If no loggers have been added for a specific context, all loggers for the
+   * Log::CATCHALL context are returned.
    *
    * @param string $context
-   * @return Logger or null if not set for this context.
+   * @return array
    */
-  public static function getLogger($context = self::GENERAL) {
-    if (!self::$isEnabled) return null;
+  public static function getLoggers($context = self::GENERAL) {
+    if ( ! self::$isEnabled) return null;
     $context = self::sanitizeContext($context);
     if (isset(self::$loggers[$context])) return self::$loggers[$context];
     elseif (isset(self::$loggers[self::CATCHALL])) return self::$loggers[self::CATCHALL];
-    else return null;
+    else return array();
   }
-
 
   /**
    * Logs a debug message in the logger for the specific context
@@ -111,12 +108,8 @@ abstract class Log {
    * @return bool true on success, false if no logger specified for the context or the logger below level.
    */
   public static function debug($message, $context = self::GENERAL, $additionalInfo = null) {
-    if ($logger = self::getLogger($context)) {
-      return $logger->debug($message, ($context === self::GENERAL || $context === self::CATCHALL) ? null : $context, $additionalInfo);
-    }
-    return false;
+    return self::doLog('debug', $message, $context, $additionalInfo);
   }
-
 
   /**
    * Logs an info message in the logger for the specific context
@@ -127,12 +120,8 @@ abstract class Log {
    * @return bool true on success, false if no logger specified for the context or the logger below level.
    */
   public static function info($message, $context = self::GENERAL, $additionalInfo = null) {
-    if ($logger = self::getLogger($context)) {
-      return $logger->info($message, ($context === self::GENERAL || $context === self::CATCHALL) ? null : $context, $additionalInfo);
-    }
-    return false;
+    return self::doLog('info', $message, $context, $additionalInfo);
   }
-
 
   /**
    * Logs a warning message in the logger for the specific context
@@ -143,12 +132,8 @@ abstract class Log {
    * @return bool true on success, false if no logger specified for the context or the logger below level.
    */
   public static function warning($message, $context = self::GENERAL, $additionalInfo = null) {
-    if ($logger = self::getLogger($context)) {
-      return $logger->warning($message, ($context === self::GENERAL || $context === self::CATCHALL) ? null : $context, $additionalInfo);
-    }
-    return false;
+    return self::doLog('warning', $message, $context, $additionalInfo);
   }
-
 
   /**
    * Logs an error message in the logger for the specific context
@@ -159,12 +144,8 @@ abstract class Log {
    * @return bool true on success, false if no logger specified for the context or the logger below level.
    */
   public static function error($message, $context = self::GENERAL, $additionalInfo = null) {
-    if ($logger = self::getLogger($context)) {
-      return $logger->error($message, ($context === self::GENERAL || $context === self::CATCHALL) ? null : $context, $additionalInfo);
-    }
-    return false;
+    return self::doLog('error', $message, $context, $additionalInfo);
   }
-
 
   /**
    * Logs a fatal error message in the logger for the specific context
@@ -175,12 +156,25 @@ abstract class Log {
    * @return bool true on success, false if no logger specified for the context or the logger below level.
    */
   public static function fatal($message, $context = self::GENERAL, $additionalInfo = null) {
-    if ($logger = self::getLogger($context)) {
-      return $logger->fatal($message, ($context === self::GENERAL || $context === self::CATCHALL) ? null : $context, $additionalInfo);
-    }
-    return false;
+    return self::doLog('fatal', $message, $context, $additionalInfo);
   }
 
+  /**
+   * Calls the specified method on each logger for the specified context provided.
+   *
+   * @param string $message debug | info | warning | error | fatal
+   * @param string $method the method to call on the loggers.
+   * @param string $context
+   * @param array $additionalInfo
+   * @return bool true on success, false if no logger specified for the context or the logger below level.
+   */
+  protected static function doLog($method, $message, $context, $additionalInfo) {
+    $logged = false;
+    foreach (self::getLoggers($context) as $logger) {
+      $logged = $logger->$method($message, ($context === self::GENERAL || $context === self::CATCHALL) ? null : $context, $additionalInfo) || $logged;
+    }
+    return $logged;
+  }
 
   /**
    * Sanitizes a context.
@@ -192,7 +186,6 @@ abstract class Log {
     if ($context === self::GENERAL || $context === self::CATCHALL) return $context;
     return strtolower($context);
   }
-
 
 }
 
--- library/Logger/Logger.php
+++ library/Logger/Logger.php
@@ -6,17 +6,16 @@
  * @author Matthias Loitsch <developer@ma.tthias.com>
  * @copyright Copyright (c) 2010, Matthias Loitsch
  * @package Logger
- **/
-
+ */
 /**
  * Including the exceptions
  */
-if (!class_exists('LoggerException', false)) include(dirname(__FILE__) . '/LoggerExceptions.php');
+if ( ! class_exists('LoggerException', false)) include(dirname(__FILE__) . '/LoggerExceptions.php');
 
 /**
  * Including the static class Log
  */
-if (!class_exists('Log', false)) include(dirname(__FILE__) . '/Log.php');
+if ( ! class_exists('Log', false)) include(dirname(__FILE__) . '/Log.php');
 
 /**
  * Loggers have to extend this class
@@ -24,21 +23,19 @@ if (!class_exists('Log', false)) include(dirname(__FILE__) . '/Log.php');
  * @author Matthias Loitsch <developer@ma.tthias.com>
  * @copyright Copyright (c) 2010, Matthias Loitsch
  * @package Logger
- **/
+ */
 abstract class Logger {
-
-  /**#@+
+  /*   * #@+
    * The different Log levels.
    *
    * @var int
    */
-  const DEBUG   = 0x00001;
-  const INFO    = 0x00011;
+  const DEBUG = 0x00001;
+  const INFO = 0x00011;
   const WARNING = 0x00111;
-  const ERROR   = 0x01111;
-  const FATAL   = 0x11111;
-  /**#@-*/
-
+  const ERROR = 0x01111;
+  const FATAL = 0x11111;
+  /*   * #@- */
 
   /**
    * The minimum level a message has to have to be logged.
@@ -47,7 +44,6 @@ abstract class Logger {
    * @var int
    */
   protected $level = self::WARNING;
-
 
   /**
    * Sets the level for logging.
@@ -79,7 +75,6 @@ abstract class Logger {
     return ((int) $messageLevel & $this->level) === $this->level;
   }
 
-
   /**
    * Log a debug message
    * 
@@ -88,7 +83,9 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    * @return bool true on success, false if nothing has been logged.
    */
-  public function debug($message, $context = null, $additionalInfo = null) { return $this->log($message, self::DEBUG, $context, $additionalInfo); }
+  public function debug($message, $context = null, $additionalInfo = null) {
+    return $this->log($message, self::DEBUG, $context, $additionalInfo);
+  }
 
   /**
    * Log an info message
@@ -98,7 +95,9 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    * @return bool true on success, false if nothing has been logged.
    */
-  public function info($message, $context = null, $additionalInfo = null) { return $this->log($message, self::INFO, $context, $additionalInfo); }
+  public function info($message, $context = null, $additionalInfo = null) {
+    return $this->log($message, self::INFO, $context, $additionalInfo);
+  }
 
   /**
    * Log a warning
@@ -108,8 +107,9 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    * @return bool true on success, false if nothing has been logged.
    */
-  public function warning($message, $context = null, $additionalInfo = null) { return $this->log($message, self::WARNING, $context, $additionalInfo); }
-
+  public function warning($message, $context = null, $additionalInfo = null) {
+    return $this->log($message, self::WARNING, $context, $additionalInfo);
+  }
 
   /**
    * Alias for warning
@@ -119,8 +119,9 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    * @return bool true on success, false if nothing has been logged.
    */
-  public function warn($message, $context = null, $additionalInfo = null) { return $this->warning($message, $context, $additionalInfo); }
-
+  public function warn($message, $context = null, $additionalInfo = null) {
+    return $this->warning($message, $context, $additionalInfo);
+  }
 
   /**
    * Log an error
@@ -130,7 +131,9 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    * @return bool true on success, false if nothing has been logged.
    */
-  public function error($message, $context = null, $additionalInfo = null) { return $this->log($message, self::ERROR, $context, $additionalInfo); }
+  public function error($message, $context = null, $additionalInfo = null) {
+    return $this->log($message, self::ERROR, $context, $additionalInfo);
+  }
 
   /**
    * Log a fatal error
@@ -140,8 +143,9 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    * @return bool true on success, false if nothing has been logged.
    */
-  public function fatal($message, $context = null, $additionalInfo = null) { return $this->log($message, self::FATAL, $context, $additionalInfo); }
-
+  public function fatal($message, $context = null, $additionalInfo = null) {
+    return $this->log($message, self::FATAL, $context, $additionalInfo);
+  }
 
   /**
    * The actual method that logs the message
@@ -153,12 +157,11 @@ abstract class Logger {
    * @return bool true on success, false if nothing has been logged.
    */
   protected function log($message, $level, $context = null, $additionalInfo = null) {
-    if ($additionalInfo && !is_array($additionalInfo)) throw new LoggerException("Additional info has to be an array.");
-    if (!$this->shouldLog($level)) return false;
+    if ($additionalInfo && ! is_array($additionalInfo)) throw new LoggerException("Additional info has to be an array.");
+    if ( ! $this->shouldLog($level)) return false;
     $this->doLog($message, $level, $context, $additionalInfo);
     return true;
   }
-
 
   /**
    * The actual method that logs the message
@@ -169,8 +172,5 @@ abstract class Logger {
    * @param array $additionalInfo An associative array with more info. Eg: array('content'=>'Some stuff')
    */
   protected abstract function doLog($message, $level, $context, $additionalInfo);
-
-
 }
-
 

ee2a0611da8999e1e26d045da9bff0e38c6bc83f
Matthias Loitsch <m@tthias.com>
2011-07-05 19:28:41 +0200

    Updated the tag to v2.3.5

--- library/rincewind.php
+++ library/rincewind.php
@@ -10,7 +10,7 @@
 /**
  * Defines the current rincewind version
  */
-define('RINCEWIND_VERSION', 20304);
+define('RINCEWIND_VERSION', 20305);
 
 if ( ! defined('REQUIRED_RINCEWIND_VERSION')) trigger_error('You should set a REQUIRED_RINCEWIND_VERSION (' . RINCEWIND_VERSION . ').', E_USER_WARNING);
 
